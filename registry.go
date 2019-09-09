d015e78bdfbf23af1d7d07107c0e767b0e67cbf1
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-09-09 02:51:23 +0100

    Progress 6

--- registry.go
+++ registry.go
@@ -3,12 +3,15 @@ package main
 import (
 	"gopkg.in/qamarian-mmp/rxlib.v0"
 	// Import mains below ...
+	"./logger"
+	"./server"
 
 )
 
 var (
 	mains []*rxlib.Register = []*rxlib.Register {
-		// Register your mains here.
+		rxlib.NewRegister ("logger", []string {}, logger.Logger)
+		rxlib.NewRegister ("server", []string {"logger"}, server)
 	}
 	osLog rxlib.RxLog = newLog ()
 )
