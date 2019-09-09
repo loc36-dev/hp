45662a949e2a990b7c82494d94b511c955410c26
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-09-09 03:29:07 +0100

    Progress 7

---
+++ logger/README.md
@@ -0,0 +1,1 @@
+This RSM is based on the version 0.2.0 of the github.com/qamarian-rsm/logger RSM.
--- logger/provideWriter.go
+++ logger/provideWriter.go
@@ -1,29 +1,39 @@
 package logger
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"io"
+	"os"
+	_ "gopkg.in/mattn/go-sqlite3.v1"
 )
 
-// provideWriter () is called by the logger, to provide an io.Writer that logs should be
-// forwarded to.
-//
-// Do not forget to provide an implementation for this function, when using it in your
-// Rexa-based Software.
-//
-// On success, an io.Writer and a nil error should be returned. On failure, an io.Writer
-// and an error should be returned.
+// provideWriter () returns the log file of the RbS. It fetches the logfile's path from an
+// SQLite 3 database.
 func provideWriter () (io.Writer, error) {
-	// Some implementation
-	return testWriter {}, nil
-}
-
-// testWriter is just a data type placed here to faciliate the testing of this package.
-// Delete this data type when coding your implementation of provideWriter ().
-type testWriter struct {}
-
-func (t testWriter) Write (data []byte) (int, error) {
-	fmt.Print ("Package 'github.com/qamarian-rxm/logger' is being tested. Log: ")
-	fmt.Print (string (data), "\n")
-	return 1, nil
+	db, errX := sql.Open ("sqlite3", "file:res/conf.db3")
+	if errX != nil {
+		errMssg := fmt.Sprintf ("RbS's configuration file could not be opened. " +
+			"[%s]", errX.Error ())
+		return nil, errors.New (errMssg)
+	}
+	query := `SELECT filepath
+		FROM filepath
+		WHERE name == ?
+	`
+	logfilePath := ""
+	errY := db.QueryRow (query, "logfile").Scan (&logfilePath)
+	if errY != nil {
+		errMssg := fmt.Sprintf ("RbS's logfile path could not be retrieved " +
+			"from the configuration file. [%s]", errY.Error ())
+		return nil, errors.New (errMssg)
+	}
+	logfile, errZ := os.Open (logfilePath, O_WRONLY | O_APPEND | O_SYNC, 0660)
+	if errZ != nil {
+		errMssg := fmt.Sprintf ("RbS's logfile could not be opened. [%s]",
+			errZ. Error ())
+		return nil, errors.New (errMssg)
+	}
+	return logfile, nil
 }
