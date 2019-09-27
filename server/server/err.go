7438d9d8a5d56352489fb0e2f76868db1b189de4
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-09-27 04:58:47 +0100

    Progress 19

--- server/server/err.go
+++ server/server/err.go
@@ -5,44 +5,44 @@ var (
 	qotaOpr *qota.Qota   // Qota used for service errors caused by operational errors.
 )
 
-// Invalid request data Error 1 - 5
+// Invalid request data Error 0 - 4
 
-invErr1 := err.New ("No request data was provided.", 1, qotaInv.Value ())
+invErr0 := err.New ("No request data was provided.", 0, qotaInv.Value ())
 qotaInv.Grow ()
 
-invErr2 := err.New ("A location data was omited.", 1, qotaInv.Value ())
+invErr1 := err.New ("A location data was omited.", 0, qotaInv.Value ())
 qotaInv.Grow ()
 
-invErr3 := err.New ("No day was specified for location a location.", 1, qotaInv.Value ())
+invErr2 := err.New ("No day was specified for location a location.", 0, qotaInv.Value ())
 qotaInv.Grow ()
 
-invErr4 := err.New ("Data of invalid day requested for a location.", 1, qotaInv.Value ())
+invErr3 := err.New ("Data of invalid day requested for a location.", 0, qotaInv.Value ())
 qotaInv.Grow ()
 
-invErr5 := err.New ("Data of more than 256 locations may not be requested at a time."), 1, qotaInv.Value ())
+invErr4 := err.New ("Data of more than 146 locations may not be requested at a time."), 0, qotaInv.Value ())
 qotaInv.Grow ()
 
-// Operational Error 1 - 5
+// Operational Error 0 - 4
 
-oprErr1 := err.New ("Database unreachable.", 2, qotaOpr.Value ())
+oprErr0 := err.New ("Database unreachable.", 1, qotaOpr.Value ())
 qotaOpr.Grow ()
 
-oprErr2 := err.New ("Unable to check if all locations exist.", 2, qotaOpr.Value ())
+oprErr1 := err.New ("Unable to check if all locations exist.", 1, qotaOpr.Value ())
 qotaOpr.Grow ()
 
-oprErr3 := err.New ("Some locations do not exist.", 2, qotaOpr.Value ())
+oprErr2 := err.New ("Some locations do not exist.", 1, qotaOpr.Value ())
 qotaOpr.Grow ()
 
-oprErr4 := err.New ("Unable to fetch the sensor IDs of all locations.", 2, qotaOpr.Value ())
+oprErr3 := err.New ("Unable to fetch the sensor IDs of all locations.", 1, qotaOpr.Value ())
 qotaOpr.Grow ()
 
-oprErr5 := err.New ("Unable to fetch the sensor ID of a location.", 2, qotaOpr.Value ())
+oprErr4 := err.New ("Unable to fetch the sensor ID of a location.", 1, qotaOpr.Value ())
 qotaOpr.Grow ()
 
-// Operational Error 9 - 10
+// Operational Error 5 - 9
 
-oprErr6 := err.New ("Unable to fetch the states of all locations.", 2, qotaOpr.Value ())
+oprErr5 := err.New ("Unable to fetch the states of all locations.", 1, qotaOpr.Value ())
 qotaOpr.Grow ()
 
-oprErr7 := err.New ("Unable to fetch a state data.", 2, qotaOpr.Value ())
+oprErr6 := err.New ("Unable to fetch a state data.", 1, qotaOpr.Value ())
 qotaOpr.Grow ()
