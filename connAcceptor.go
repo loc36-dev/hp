81e4941eeba8944d6f32859f283af47ea6a9f951
Ibrahim Oladipupo Qamardeen <kensharval@gmail.com>
2019-10-06 23:25:30 +0100

    Progress 48

--- connAcceptor.go
+++
@@ -1,8 +0,0 @@
-package server
-
-import (
-	"gopkg.in/qamarian-mmp/rxlib.v0"
-)
-
-func ConnAcceptor (key rxlib.Key) () {
-}
---
+++ main.go
@@ -0,0 +1,8 @@
+package server
+
+import (
+	"gopkg.in/qamarian-mmp/rxlib.v0" // v0.
+)
+
+func main () {
+}
