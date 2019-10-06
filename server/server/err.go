8c56606e0211308fcf389e11be0685ab35b3d3f3
Ibrahim Oladipupo Qamardeen <kensharval@gmail.com>
2019-10-06 03:03:34 +0100

    Progress 43

--- server/server/err.go
+++ server/server/err.go
@@ -6,52 +6,74 @@ import (
 	"math/big"
 )
 
-func
-
 var (
-// Error IDs
-	invErrID *big.Int = big.NewInt (0) //
-	oprErrID *big.Int = big.NewInt (0) //
+// Errors class ID
+	invErrID *big.Int // Class ID for service errors caused by invalid request.
+	oprErrID *big.Int // Class ID for service errors caused by operational errors.
 
-// Error iotas
-	qotaInv *qota.Qota  = qota.New () // Qota used for service errors caused by invalid request.
-	qotaOpr *qota.Qota  = qota.New () // Qota used for service errors caused by operational errors.
 
-// Invalid request data Error 0 - 4
+// Errors iota
+	iotaInv *qota.Qota // Iota used for service errors caused by invalid request.
+	iotaOpr *qota.Qota // Iota used for service errors caused by operational errors.
 
-	invErr0 err.Error = err.New ("No request data was provided.", 0, uint8 (qotaInv.Value ()))
-	invErr1 err.Error = err.New ("A location data was omited.", 0, qotaInv.Value ())
-	invErr2 err.Error = err.New ("No day was specified for a location.", 0, qotaInv.Value ())
-	invErr3 err.Error = err.New ("Data of an invalid day requested for a location.", 0, qotaInv.Value ())
-	invErr4 err.Error = err.New ("Data of more than 32 locations may not be requested at a time.", 0, qotaInv.Value ())
+// Invalid request data Errors
 
-// Invalid request data Error 5 - 9
+	invErr0 *err.Error
+	invErr1 *err.Error
+	invErr2 *err.Error
+	invErr3 *err.Error
+	invErr4 *err.Error
+	invErr5 *err.Error
+	invErr6 *err.Error
+	invErr7 *err.Error
+	invErr8 *err.Error
+	invErr9 *err.Error
 
-	invErr5 err.Error = err.New ("Request data too long.", 0, qotaInv.Value ())
-	invErr6 err.Error = err.New ("Data of over 32 locations may not be requested at a tme.", 0, qotaInv.Value ())
-	invErr7 err.Error = err.New ("Records of over 32 days may not be requested for a location, at a time.", 0, qotaInv.Value ())
-	invErr8 err.Error = err.New ("The ID of a location was not provided.", 0, qotaInv.Value ())
-	invErr9 err.Error = err.New ("Request data validation failed.", 0, qotaInv.Value ())
+// Operational Errors
 
-// Operational Error 0 - 4
-
-	oprErr0 err.Error = err.New ("Database unreachable.", 1, qotaOpr.Value ())
-	oprErr1 err.Error = err.New ("Unable to check if all locations exist.", 1, qotaOpr.Value ())
-	oprErr2 err.Error = err.New ("Some locations do not exist.", 1, qotaOpr.Value ())
-	oprErr3 err.Error = err.New ("Unable to fetch the sensor IDs of all locations.", 1, qotaOpr.Value ())
-	oprErr4 err.Error = err.New ("Unable to fetch the sensor ID of a location.", 1, qotaOpr.Value ())
-
-// Operational Error 5 - 9
-
-	oprErr5 err.Error = err.New ("Unable to fetch the states of all locations.", 1, qotaOpr.Value ())
-	oprErr6 err.Error = err.New ("Unable to fetch a state data.", 1, qotaOpr.Value ())
-	oprErr7 err.Error = err.New ("Unable to make request records a squaket.", 1, qotaOpr.Value ())
-	oprErr8 err.Error = err.New ("Unable to organize request records, based on sensor.", 1, qotaOpr.Value ())
-	oprErr9 err.Error = err.New ("Unable to make records of a sensor a squaket.", 1, qotaOpr.Value ())
-
-// Operational Error 10 - 14
-
-	oprErr10 err.Error = err.New ("Unable to organize records of a sensor, based on day.", 1, qotaOpr.Value ())
-	oprErr11 err.Error = err.New ("A sensor record could not be marshalled.", 1, qotaOpr.Value ())
+	oprErr0 *err.Error
+	oprErr1 *err.Error
+	oprErr2 *err.Error
+	oprErr3 *err.Error
+	oprErr4 *err.Error
+	oprErr5 *err.Error
+	oprErr6 *err.Error
+	oprErr7 *err.Error
+	oprErr8 *err.Error
+	oprErr9 *err.Error
+	oprErr10 *err.Error
+	oprErr11 *err.Error
 
 )
+
+func init () {
+	invErrID = big.NewInt (0)
+	oprErrID = big.NewInt (1)
+
+	iotaInv = qota.New ()
+	iotaOpr = qota.New ()
+
+	invErr0 = err.New ("No request data was provided.", invErrID, iotaInv.Value ())
+	invErr1 = err.New ("A location data was omited.", invErrID, iotaInv.Value ())
+	invErr2 = err.New ("No day was specified for a location.", invErrID, iotaInv.Value ())
+	invErr3 = err.New ("Data of an invalid day requested for a location.", invErrID, iotaInv.Value ())
+	invErr4 = err.New ("Data of more than 32 locations may not be requested at a time.", invErrID, iotaInv.Value ())
+	invErr5 = err.New ("Request data too long.", invErrID, iotaInv.Value ())
+	invErr6 = err.New ("Data of over 32 locations may not be requested at a tme.", invErrID, iotaInv.Value ())
+	invErr7 = err.New ("Records of over 32 days may not be requested for a location, at a time.", invErrID, iotaInv.Value ())
+	invErr8 = err.New ("The ID of a location was not provided.", invErrID, iotaInv.Value ())
+	invErr9 = err.New ("Request data validation failed.", invErrID, iotaInv.Value ())
+
+	oprErr0 = err.New ("Database unreachable.", oprErrID, iotaOpr.Value ())
+	oprErr1 = err.New ("Unable to check if all locations exist.", oprErrID, iotaOpr.Value ())
+	oprErr2 = err.New ("Some locations do not exist.", oprErrID, iotaOpr.Value ())
+	oprErr3 = err.New ("Unable to fetch the sensor IDs of all locations.", oprErrID, iotaOpr.Value ())
+	oprErr4 = err.New ("Unable to fetch the sensor ID of a location.", oprErrID, iotaOpr.Value ())
+	oprErr5 = err.New ("Unable to fetch the states of all locations.", oprErrID, iotaOpr.Value ())
+	oprErr6 = err.New ("Unable to fetch a state data.", oprErrID, iotaOpr.Value ())
+	oprErr7 = err.New ("Unable to make request records a squaket.", oprErrID, iotaOpr.Value ())
+	oprErr8 = err.New ("Unable to organize request records, based on sensor.", oprErrID, iotaOpr.Value ())
+	oprErr9 = err.New ("Unable to make records of a sensor a squaket.", oprErrID, iotaOpr.Value ())
+	oprErr10 = err.New ("Unable to organize records of a sensor, based on day.", oprErrID, iotaOpr.Value ())
+	oprErr11 = err.New ("A sensor record could not be marshalled.", oprErrID, iotaOpr.Value ())
+}
